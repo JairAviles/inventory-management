95fd978829b38767ccb209390ba75bd7409a10e9
Jair Avilés <hi@jairaviles.mx>
2018-07-12 21:45:33 -0700

    refactor tests definition

--- src/test/java/com/inventory/test/unit/DontRequireBeforeItemTest.java
+++ src/test/java/com/inventory/test/unit/DontRequireBeforeItemTest.java
@@ -3,37 +3,20 @@ package com.inventory.test.unit;
 import com.inventory.backend.repositories.ItemSingleton;
 import com.inventory.backend.service.ItemService;
 import com.inventory.exceptions.ItemException;
-import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ItemTest {
+public class DontRequireBeforeItemTest {
 
     private ItemSingleton itemSingleton;
 
     @Before
     public void init() {
         itemSingleton = ItemSingleton.getInstance();
-        ItemService.create("create Test01 10.0 15.0");
-        ItemService.create("create Test02 8.0 13.0");
-        ItemService.create("create Test03 4.0 7.0");
-    }
-
-    @After
-    public void finish() {
-        ItemService.delete("delete Test01");
-        ItemService.delete("delete Test02");
-        ItemService.delete("delete Test03");
     }
 
     @Test(expected = ItemException.class)
     public void shouldThrownItemExceptionWhenCreateNewItemHasWrongParams() {
-        ItemService.create("create Test01 20.0 22.5");
-    }
-
-    @Test(expected = ItemException.class)
-    public void shouldThrownItemExceptionWhenCreateNewItemHasExistingItem() {
         ItemService.create("create WrongParamsPassed");
     }
 
@@ -42,11 +25,9 @@ public class ItemTest {
         ItemService.create("create Wrong Params Passed");
     }
 
-    @Test
-    public void shouldUpdateBuySuccessfully() {
-        ItemService.updateBuy("updateBuy Test01 10");
-
-        Assert.assertEquals(10, itemSingleton.currentItemMap.get("Test01").getQuantity());
+    @Test(expected = ItemException.class)
+    public void shouldThrownItemExceptionWhenUpdateBuyItemDoesNotExists() {
+        ItemService.updateBuy("updateBuy NonExistingItem 10");
     }
 
     @Test(expected = ItemException.class)
@@ -54,15 +35,9 @@ public class ItemTest {
         ItemService.updateBuy("updateBuy WrongParamsPassed");
     }
 
-    @Test
-    public void shouldUpdateSellSuccessfully() {
-        ItemService.updateBuy("updateBuy Test01 10");
-
-        Assert.assertEquals(10, itemSingleton.currentItemMap.get("Test01").getQuantity());
-
-        ItemService.updateSell("updateSell Test01 10");
-
-        Assert.assertEquals(0, itemSingleton.currentItemMap.get("Test01").getQuantity());
+    @Test(expected = ItemException.class)
+    public void shouldThrownItemExceptionWhenUpdateSellItemDoesNotExists() {
+        ItemService.updateSell("updateSell NonExistingItem 2");
     }
 
     @Test(expected = ItemException.class)
@@ -70,5 +45,13 @@ public class ItemTest {
         ItemService.updateSell("updateSell WrongParamsPassed");
     }
 
+    @Test(expected = ItemException.class)
+    public void shouldThrownItemExceptionWhenNoDataIsRegisteredOnShowingReport() {
+        ItemService.report();
+    }
 
+    @Test(expected = ItemException.class)
+    public void shouldThrownItemExceptionWhenDeleteItemIsNotRegistered() {
+        ItemService.delete("delete Test01");
+    }
 }
